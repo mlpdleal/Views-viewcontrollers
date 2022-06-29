638fde0f27ac3b241513079e7ed238fe87f4752b
mlpdleal <manoellealdeveloper@gmail.com>
2022-06-29 20:55:40 -0300

    Completed Day 65 of 100 Days of SwiftUI

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Manoel Leal on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
