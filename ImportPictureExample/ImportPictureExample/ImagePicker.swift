c980ae591217a23870dfd98d51f6fcd26374effe
mlpdleal <manoellealdeveloper@gmail.com>
2022-06-29 19:10:44 -0300

    Completed Day 64 of 100 Days of SwiftUI

--- ImportPictureExample/ImportPictureExample/ImagePicker.swift
+++ ImportPictureExample/ImportPictureExample/ImagePicker.swift
@@ -10,11 +10,38 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable{
+    
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker){
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self){
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+            
+            
+        }
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -22,5 +49,7 @@ struct ImagePicker: UIViewControllerRepresentable{
         
     }
     
-    //typealias UIViewControllerType = PHPickerViewController
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 }
