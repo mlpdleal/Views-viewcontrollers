b5c312a6cf937fdb45bde242edd7a5fdeb983235
mlpdleal <manoellealdeveloper@gmail.com>
2022-07-04 19:55:40 -0300

    Completed Day 70 of 100 Days of SwiftUI

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Manoel Leal on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
