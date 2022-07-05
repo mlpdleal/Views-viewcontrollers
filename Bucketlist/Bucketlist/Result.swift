48d3191805f19eec6d95f3bc59e3bf552446b01f
mlpdleal <manoellealdeveloper@gmail.com>
2022-07-05 18:32:02 -0300

    Completed Day 71 of 100 Days of SwiftUI

---
+++ Bucketlist/Bucketlist/Result.swift
@@ -0,0 +1,30 @@
+//
+//  Result.swift
+//  Bucketlist
+//
+//  Created by Manoel Leal on 05/07/22.
+//
+
+import Foundation
+
+struct Result: Codable{
+    let query: Query
+}
+
+struct Query: Codable{
+    let pages: [Int: Page]
+}
+
+struct Page: Codable, Comparable{
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+    
+    var description: String{
+        terms?["description"]?.first ?? "No further information"
+    }
+    
+    static func <(lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
+}
